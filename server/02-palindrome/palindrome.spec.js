4c74c9e099261c1aa5b3a998fd1d97ab5f47b050
g-ishi <al14006@shibaura-it.ac.jp>
2021-08-31 21:20:49 +0900

    Added guard statement for non-string

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,5 +1,6 @@
 // テスト対象のコード
 let isPalindrome = phrase => {
+  if (typeof phrase !== 'string') throw new Error("input must be a string");
   if (phrase === '') throw new Error("empty strings are not palindromes");
   if (phrase.trim() === '') return false;
   return phrase === phrase.split('').reverse().join('');
@@ -29,5 +30,9 @@ describe('the palindrome canary spec', () => {
       isPalindrome("");
     }).toThrowError("empty strings are not palindromes");
   });
-  it.todo('error for not a string');
+  it('error for not a string', () => {
+    expect(() => {
+      isPalindrome(121);
+    }).toThrowError("input must be a string");
+  });
 });
