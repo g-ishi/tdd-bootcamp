dd6b99104bc353194fd4652a6d06d06913081749
g-ishi <al14006@shibaura-it.ac.jp>
2021-09-01 23:40:27 +0900

    starts empty test case

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,3 +1,9 @@
+// テスト用の関数定義
+let stack = {
+  isEmpty: () => true
+}
+
+
 describe('the stack canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
@@ -5,7 +11,9 @@ describe('the stack canary spec', () => {
 });
 
 describe('a stack', () => {
-  it.todo('starts empty');
+  it('starts empty', () => {
+    expect(stack.isEmpty()).toBe(true);
+  });
   it.todo('starts with stack size of 0');
   it.todo('is not empty when pushed');
   it.todo('stack size is 1 when pushed');
